28df8b623ab8b5194b88ea3635a1271939d16c94
Chupakabra0 <rootalfa4@gmail.com>
2021-09-20 22:38:26 +0300

    Create RelayCommand.cs

---
+++ VedroidLab2/VedroidLab2/Commands/RelayCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace VedroidLab2.Commands {
+    class RelayCommand : ICommand {
+        public RelayCommand(Action action, Func<bool> canExecute = null) {
+            this.action_     = action;
+            this.canExecute_ = canExecute;
+        }
+
+        public bool CanExecute(object parameter) =>
+            this.canExecute_?.Invoke() ?? true;
+
+        public void Execute(object parameter) =>
+            this.action_?.Invoke();
+
+        public event EventHandler CanExecuteChanged = (sender, args) => {};
+
+        protected Action     action_;
+        protected Func<bool> canExecute_;
+    }
+}
