10608299e303e38f3cb11588e1770ee80b481e61
Chupakabra0 <rootalfa4@gmail.com>
2021-09-20 22:38:17 +0300

    Create BaseViewModel.cs

---
+++ VedroidLab2/VedroidLab2/ViewModels/BaseViewModel/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using PropertyChanged;
+
+namespace VedroidLab2.ViewModels {
+    /// <summary>
+    ///     Base view-model with implementation of INotifyPropertyChanged interface
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    abstract class BaseViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        ///     Inform view, that some property have changed
+        /// </summary>
+        /// <param name="propertyName">
+        ///     Caller property name
+        /// </param>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
